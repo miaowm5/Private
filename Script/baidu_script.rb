da57e1d798856f294fd246091cb2f2d81b8c29ea
mwm5 <miaowm5@users.noreply.github.com>
2015-01-23 23:52:50 +0800

    Update baidu_script.rb

--- Script/baidu_script.rb
+++ Script/baidu_script.rb
@@ -1,43 +1,42 @@
 class Window_introduce < Window_Base
   def initialize
     super(80,80,640-160,480-160)
-    self.back_opacity = 160                               #
-    self.opacity = 160      #这里都是透明度，
-    self.contents_opacity = 255                       #
-    self.contents = Bitmap.new(width - 32, height - 32) #这个不管他，照搬
-
-  end
-
-  #--------------------------------------------------------------------------
-  # ● 设置文本
-  #     text  : 窗口显示的字符串
-  #     align : 对齐方式 (0..左对齐、1..中间对齐、2..右对齐)
-  #--------------------------------------------------------------------------
+    self.back_opacity = 160
+    self.opacity = 160
+    self.contents_opacity = 255
+    self.contents = Bitmap.new(width - 32, height - 32)
+  end  
   def set_text(text, align = 0)
-    if text != @text or align != @align
-      # 再描绘文本
+    if text != @text or align != @align      
       self.contents.clear
-      self.contents.font.color = normal_color
+      self.contents.font.color = normal_color      
+      # 获取描绘区域的属性
+      # x,y：当前文字描绘的位置
+      # width,height：描绘文字区域的宽度和高度
+      # new_x：每行文字的开始x坐标
       pos = {
         :x => 0,:y => 0,
         :new_x => 0,:width => 500,:height => 32,
       }
-      text = text.scan(/./)
-      text.each {|c| process_character(c,pos) }
-      @text = text
-      @align = align
-      @actor = nil
+      # 按照顺序依次描绘文字
+      text.scan(/./).each {|c| process_character(c,pos) }
+      @text,@align = text,align      
     end
     self.visible = true
   end
-  def process_character(c,pos)
-    size = self.contents.text_size(c)    
+  def process_character(c,pos)    
     case c
+    # 若文字为“/”则换行
     when '/'
       pos[:x] = pos[:new_x]
-      pos[:y] += size.height
+      pos[:y] += pos[:height]
+    # 处理正常文字
     else
-      self.contents.draw_text(pos[:x],pos[:y],500,32, c)
+      # 获取文字大小
+      size = self.contents.text_size(c)
+      # 描绘文字
+      self.contents.draw_text(pos[:x],pos[:y],pos[:width],pos[:height], c)
+      # 计算下一个文字的描绘位置
       pos[:x] += size.width
     end
   end
